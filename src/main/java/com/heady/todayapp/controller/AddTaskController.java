83525caeeb460ed828e419243f996bc44c1e1a26
Edward <edward.gratefuldead1.com>
2022-09-09 12:41:41 -0500

    Added thymeleaf layouts for header and footer. Created login page with CSS styling

--- src/main/java/com/heady/todayapp/controller/AddTaskController.java
+++ src/main/java/com/heady/todayapp/controller/AddTaskController.java
@@ -1,4 +1,20 @@
 package com.heady.todayapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/createTask")
 public class AddTaskController {
+
+    @GetMapping("/")
+    public String display(Model model){
+        model.addAttribute("messageCreateTask", "Create A Task");
+
+        return "createTask";
+    }
+
+
 }
