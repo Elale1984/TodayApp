a7d752cea3031f5a65c873cc970ec5594eaf5835
elalejr <javaeddie84@gmail.com>
2022-09-27 10:18:18 -0500

    Changing to cloud

--- src/main/java/com/heady/todayapp/model/Task.java
+++ src/main/java/com/heady/todayapp/model/Task.java
@@ -5,10 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
+
 public class Task {
 
     int id;
@@ -16,4 +14,59 @@ public class Task {
     String starttime;
     String date;
     String details;
+    
+    public Task() {
+    
+    }
+    
+	public Task(int id, String title, String starttime, String date, String details) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.starttime = starttime;
+		this.date = date;
+		this.details = details;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getStarttime() {
+		return starttime;
+	}
+
+	public void setStarttime(String starttime) {
+		this.starttime = starttime;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
+    
+    
 }
--- src/main/java/com/heady/todayapp/model/User.java
+++ src/main/java/com/heady/todayapp/model/User.java
@@ -1,16 +1,35 @@
 package com.heady.todayapp.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@AllArgsConstructor
-@Setter
-@Getter
-@NoArgsConstructor
 public class User {
     String username;
     String password;
+    
+    public User() {
+    	
+    }
+    
+	public User(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
 }
