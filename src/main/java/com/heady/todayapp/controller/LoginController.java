b0bb621ae903ad22a9d8334e63b02f84efe87d4f
elalejr <javaeddie84@gmail.com>
2022-10-24 15:34:35 -0500

    no message

--- src/main/java/com/heady/todayapp/controller/LoginController.java
+++ src/main/java/com/heady/todayapp/controller/LoginController.java
@@ -26,7 +26,7 @@ public class LoginController {
     }
     @GetMapping("/error")
     public String failLogin(User user, Model model) {
-        model.addAttribute("welcomeMessage", "Login Failed, try again!");
+        model.addAttribute("welcomeMessage", "Login Failed, please try again");
         model.addAttribute("loginData", new User());
         return "login";
     }
